2bd0ac67aef97f59477bc53292d5992dcfafdff4
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2021-12-09 14:52:39 +0000

    illustrate normalisation

--- singlecell_seurat_1/preprocess.R
+++ singlecell_seurat_1/preprocess.R
@@ -15,3 +15,22 @@ seurat_object <- CreateSeuratObject(
     min.cells = 3,
     min.features = 200
 )
+
+# PercentageFeatureSet ----
+
+seurat_object[["percent_mt"]] <- PercentageFeatureSet(
+    seurat_object, pattern = "^MT-")
+
+# subset ----
+
+seurat_object <- subset(
+    x = seurat_object,
+    subset = nCount_RNA > 4500 & percent_mt < 15 & nFeature_RNA > 1500
+)
+
+# NormalizeData ----
+
+seurat_object <- NormalizeData(
+    object = seurat_object,
+    normalization.method = "LogNormalize"
+)
