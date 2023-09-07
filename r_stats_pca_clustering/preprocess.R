f4ea55babed70685d753d7a548d18bb24518534d
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-09-07 14:05:41 +0100

    pca loadings

--- r_stats_pca_clustering/preprocess.R
+++ r_stats_pca_clustering/preprocess.R
@@ -4,4 +4,11 @@ airway$dex <- relevel(airway$dex, "untrt")
 airway <- as(airway, "SingleCellExperiment")
 
 airway <- scater::logNormCounts(x = airway)
-reducedDim(airway, "PCA") <- prcomp(t(assay(airway, "logcounts")), 8)$x
+
+pca_use_rows <- assay(airway, "logcounts") %>% 
+  rowVars() %>% 
+  order(decreasing = TRUE) %>% 
+  head(500)
+
+airway_pca <- prcomp(t(assay(airway, "logcounts")[pca_use_rows, ]), 8)
+reducedDim(airway, "PCA") <- airway_pca$x
