4017fc89fa9fe09cc6a304a70dd177da6a5a2422
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2025-04-26 08:18:29 +0100

    Create solution.sh with exercise 2

---
+++ git_version_control_individual/solution.sh
@@ -0,0 +1,8 @@
+## Exercise 2
+
+- Create a folder called `git` in your course folder `/project/<sso>`.
+- Clone your new GitHub repository into this folder.
+- Copy your downloads.txt file to this folder.
+- Check the status od the repository.
+- Add the downloads.txt file to the staging area.
+- Commit the changes with a suitable commit message.
